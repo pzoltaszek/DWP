5072d19967696fc6256b059a0cc815a9d694c717
Piotr <31005165+pzoltaszek@users.noreply.github.com>
2017-11-19 11:25:18 +0100

    Add files via upload

--- src/Bean/Data.java
+++ src/Bean/Data.java
@@ -1,5 +1,8 @@
 package pz.strona.Bean;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Data {
 
 	private String data;
@@ -11,5 +14,14 @@ public class Data {
 	public void setData(String data) {
 		this.data = data;
 	}
-	
+
+	public String getAktualnaData() {
+
+		Date da = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+		String biezacaData = sdf.format(da);
+		return biezacaData;
+
+	}
+
 }
---
+++ src/Bean/Wpis.java
@@ -0,0 +1,31 @@
+package pz.strona.Bean;
+
+import java.util.ArrayList;
+
+public class Wpis {
+
+	private ArrayList<String> nickL = new ArrayList<String>();
+	private ArrayList<String> trescL = new ArrayList<String>();
+	private ArrayList<String> dataL = new ArrayList<String>();
+	
+	public ArrayList<String> getNickL() {
+		return nickL;
+	}
+	public void setNickL(ArrayList<String> nickL) {
+		this.nickL = nickL;
+	}
+	public ArrayList<String> getTrescL() {
+		return trescL;
+	}
+	public void setTrescL(ArrayList<String> trescL) {
+		this.trescL = trescL;
+	}
+	public ArrayList<String> getDataL() {
+		return dataL;
+	}
+	public void setDataL(ArrayList<String> dataL) {
+		this.dataL = dataL;
+	}
+	
+	
+}
